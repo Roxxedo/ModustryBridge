67981c2a9237439206c48c0b6f2f98b15a6cc31b
Pedro Ambrosini <pedroalabousas@gmail.com>
2026-06-22 18:20:11 -0300

    chore: cleanup code from template

---
+++ src/br/com/roxcom/modustrybridge/ModustryBridge.java
@@ -0,0 +1,10 @@
+package br.com.roxcom.modustrybridge;
+
+import arc.util.Log;
+import mindustry.mod.Mod;
+
+public class ModustryBridge extends Mod {
+    public ModustryBridge() {
+        Log.info("[Modustry Bridge] Modustry Bridge is starting...");
+    }
+}
